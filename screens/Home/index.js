bf7c8068a53778c2c0cf89e38095fce0a15f1746
Yellow987 <daryldsouza123@gmail.com>
2020-11-23 22:20:25 -0500

    game time

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,16 +1,65 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import styles from './styles';
-import { Header } from '../../components'
+import { Header } from '../../components';
+import { Image, TouchableOpacity } from "react-native";
 
 export default class Home extends Component {
- render() {
-   return (
-     <View style={styles.container}>
-         <Header />
-     </View>
-   );
- }
+    state = {
+        isSoundOn: true
+    };
+
+
+    onPlayPress = () => {
+        console.log("onPlayPress event handler");
+    };
+
+    onLeaderboardPress = () => {
+        console.log("onPlayPress event handler");
+    };
+
+    onToggleSound = () => {
+        this.setState({ isSoundOn: !this.state.isSoundOn });
+    }
+
+    render() {
+        const imageSource = this.state.isSoundOn
+        ? require("../../assets/icons/speaker-on.png")
+        : require("../../assets/icons/speaker-off.png");
+        return (
+            <View style={styles.container}>
+                <Header />
+                <TouchableOpacity onPress={this.onPlayPress} 
+                style={{ flexDirection: 'row', alignItems: 'center', marginTop: 80 }}>
+                    <Image source={require("../../assets/icons/play_arrow.png")} style={styles.playIcon}/>
+                    <Text style={styles.play}>PLAY!</Text>
+                </TouchableOpacity>  
+                <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 20 }}>
+                    <Image source={require("../../assets/icons/trophy.png")} style={styles.trophyIcon}/>
+                    <Text style={styles.hiscore}>Hi-score: 0</Text>
+                </View>
+                <TouchableOpacity onPress={this.onLeaderboardPress} 
+                style={{ flexDirection: 'row', alignItems: 'center', marginTop: 20 }}>
+                    <Image source={require("../../assets/icons/leaderboard.png")} style={styles.leaderboardIcon}/>
+                    <Text style={styles.leaderboard}>leaderboard</Text>
+                </TouchableOpacity>
+                <View style={styles.bottomContainer}>
+                    <Text style={[styles.copyrightText, { color: "#E64C3C" }]}>
+                        Music: Komiku
+                    </Text>
+                    <Text style={[styles.copyrightText, { color: "#F1C431" }]}>
+                        SFX: SubspaceAudio
+                    </Text>
+                    <Text style={[styles.copyrightText, { color: "#3998DB" }]}>
+                        Development: RisingStack
+                    </Text>
+                </View>
+                <View style={{ flex: 1 }} />
+                <TouchableOpacity onPress={this.onToggleSound}>
+                    <Image source={imageSource} style={styles.soundIcon} />
+                </TouchableOpacity>
+            </View>
+        );
+    }
 }
 
-// notice the deleted styles - they are imported from the styles.js!
--- screens/Home/styles.js
+++ screens/Home/styles.js
@@ -6,5 +6,53 @@ export default StyleSheet.create({
    backgroundColor: "#0a0a0a",
    justifyContent: "center",
    alignItems: "center"
- }
+ },
+ play: {
+    fontSize: 45,
+    fontFamily: "dogbyte",
+    color: "#ecf0f1",
+    marginTop: 5
+  },
+  playIcon: {
+    height: 60,
+    width: 60,
+    marginRight: 15
+  },
+  hiscore: {
+    fontSize: 28.5,
+    fontFamily: "dogbyte",
+    color: "#ecf0f1",
+    marginTop: 5
+  },
+  trophyIcon: {
+    height: 45,
+    width: 45,
+    marginRight: 12.5
+  },
+  leaderboard: {
+      fontSize: 35,
+      fontFamily: "dogbyte",
+      color: "#ecf0f1",
+      marginTop: 5
+  },
+  leaderboardIcon: {
+    height: 50,
+    width: 50,
+    marginRight: 10
+  },
+  bottomContainer: {
+   position: "absolute",
+   left: 15,
+   right: 15,
+   bottom: 12.5 // the 2.5px bottom margin from the text is subtracted from the 15px spacing
+ },
+ copyrightText: {
+   fontSize: 16,
+   fontFamily: "dogbyte",
+   marginBottom: 2.5
+ },
+ soundIcon: {
+    height: 70,
+    width: 70
+  }
 });
